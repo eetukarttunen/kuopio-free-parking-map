b16dd29b0dcb0c47cedecb13bd87887a1ec05c37
Eetu <eetu@mac.local>
2025-04-03 07:31:04 +0300

    feat: added kuopio map without styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { MapContainer, TileLayer } from 'react-leaflet';
+
+import "leaflet/dist/leaflet.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const latitude = 62.8938994150319;
+  const longitude = 27.676290599660508;
+  const mapOptions = {
+    center: [latitude, longitude],
+    zoom: 13,
+    maxZoom: 18,
+    minZoom: 5,
+  };
 
   return (
     <>
+      <MapContainer {...mapOptions} style={{ height: '500px' }}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </MapContainer>
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,18 +30,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
